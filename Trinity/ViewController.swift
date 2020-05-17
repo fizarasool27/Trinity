6fbf894801f0d3368c4cab6f28cb0003f589a80b
fizarasool27 <fizafr2127@gmail.com>
2020-05-18 05:02:56 +0530

    Initial Commit

---
+++ Trinity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trinity
+//
+//  Created by Fiza Rasool on 18/05/20.
+//  Copyright © 2020 Fiza Rasool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
